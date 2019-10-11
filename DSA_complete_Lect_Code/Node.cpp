2209a5e40c0a7f01ce6ebaa6ca19df821440fdb7
mohammad ali ashraf <ranaaliashraf12@gmail.com>
2019-10-11 20:41:18 +0500

    adding node class

---
+++ DSA_complete_Lect_Code/Node.cpp
@@ -0,0 +1,30 @@
+
+#include<iostream>
+using namespace std;
+class Node
+{
+private:
+	int data;
+public:
+	Node *next;
+
+	Node(int d) :data(d) {}
+
+	Node& setData(int d)
+	{
+		this->data = d;
+		return *this;
+	}
+
+	int getData()const
+	{
+		return data;
+	}
+
+	void print()const
+	{
+		cout << data << " ";
+	}
+
+
+};
