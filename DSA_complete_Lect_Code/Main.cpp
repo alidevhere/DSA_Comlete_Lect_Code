e391d9ca0b8a88db9faaa1849a823da8e83c496d
Muhammad Arbaz <arbazsheraz05555@gmail.com>
2020-10-02 20:21:04 -0700

    added some additional features and fixed some bugs

--- DSA_complete_Lect_Code/Main.cpp
+++ DSA_complete_Lect_Code/Main.cpp
@@ -4,11 +4,16 @@
 using namespace std;
 int main()
 {
-	Stack stack1();//calling default constructor of stack
-	Node *ptr1 =new Node();//calling default constructor of node
-	Node *ptr2 = new Node(10);//calling parameterized constructor of node
-	Stack stack2(ptr1);//prameterized constructor adding empty node
-	Stack stack3(ptr2);//prameterized constructor adding initialized node
+	//calling default constructor of stack
+	Stack stack1();
+	//calling default constructor of node
+	Node *ptr1 =new Node();
+	//calling parameterized constructor of node
+	Node *ptr2 = new Node(10);
+	//prameterized constructor adding empty node
+	Stack stack2(ptr1);
+	//prameterized constructor adding initialized node
+	Stack stack3(ptr2);
 	ptr1 = new Node(20);
 	stack3.push(ptr1);
 	Stack stack4 = stack3;
